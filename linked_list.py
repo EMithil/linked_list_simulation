75257d78e278e8336198331063d2d0deb654282b
E Mithil <147174890+EMithil@users.noreply.github.com>
2025-09-14 17:28:34 +0530

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,43 @@
+# linked_list.py
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, key):
+        temp = self.head
+        if temp and temp.data == key:
+            self.head = temp.next
+            return
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp:
+            prev.next = temp.next
+
+    def display(self):
+        temp = self.head
+        result = []
+        while temp:
+            result.append(temp.data)
+            temp = temp.next
+        print("LinkedList:", result)
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert(37)
+    ll.insert(34)
+    ll.insert(43)
+    ll.display()
+    ll.delete(37)
+    ll.display()
